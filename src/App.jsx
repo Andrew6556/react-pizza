ff961fb37a8034d2d94b3dbe78221988bfa69ed4
Andrey <andre.kozloff2004@gmail.com>
2023-10-11 15:32:01 +0300

    When loading, a loading template is shown

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 import Header from './Components/Header';
 import Categories from './Components/Categories';
 import PizzaBlock from './Components/PizzaBlock';
+import Skeleton from './Components/PizzaBlock/Skeleton';
 import Sort from './Components/Sort';
+
+// import { BrowserRouter , Router, Route ,Link, Routes} from "react-router-dom";
 
 import './scss/app.scss';
 
 function App() {
-	const [items, setItems] = React.useState([])
+	const [items, setItems]         = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
 
 	React.useEffect(() => {
 		const getPizza = async () =>{
 			setItems(await(await fetch("https://65255e5567cfb1e59ce72930.mockapi.io/items")).json())
+			setIsLoading(false)
 		}
 		getPizza()
 	}, [])
@@ -32,16 +37,18 @@ function App() {
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
 						{
-							items.map(pizza =>(
-								<PizzaBlock 
-									key   = {pizza.id} 
-									title = {pizza.title} 
-									price = {pizza.price} 
-									img	  = {pizza.imageUrl}
-									sizes = {pizza.sizes}
-									types = {pizza.types}
-								/>
-							))
+							isLoading 
+								?[...new Array(6)].map((item, itemIndex) => <Skeleton key={itemIndex} />)
+								: items.map(pizza => (
+									<PizzaBlock 
+										key   = {pizza.id} 
+										title = {pizza.title} 
+										price = {pizza.price} 
+										img	  = {pizza.imageUrl}
+										sizes = {pizza.sizes}
+										types = {pizza.types}
+									/>
+								))
 						}
 					</div>
 				</div>
