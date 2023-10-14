fc18ec11deba6d25c808289a507fd8fa211bde90
Andrey <andre.kozloff2004@gmail.com>
2023-10-15 01:15:05 +0300

    append new component Search

---
+++ src/Components/Search/index.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+import styles from "./Search.module.scss"
+
+const Search = () => {
+    return (
+        <input className={styles.input} placeholder='Поиск пиццы...'/>
+    )
+}
+export default Search
