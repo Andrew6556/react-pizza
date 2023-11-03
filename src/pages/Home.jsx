1b186aa2ad16cd3ca9d40fddeeb5ae64ac6a3c2c
Andrey <andre.kozloff2004@gmail.com>
2023-11-04 00:21:41 +0300

    did superficial processing of request errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,18 +35,28 @@ const Home = () => {
     ));
 
     const skeleton = [...new Array(6)].map((item, itemIndex) => <Skeleton key={itemIndex} />)
-    
-	React.useEffect(() => {
+
+    React.useEffect(() => {
         setIsLoading(true)
+
         const category = categoryId > 0 ? `category=${categoryId}`: "";
         const search   = searchValue ? `&search=${searchValue}`:"";
         const url = `https://65255e5567cfb1e59ce72930.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sort.typeSort}&order=desc${search}`
-
-        axios.get(url)
-            .then(getPizzas => {
-                setItems(getPizzas.data)
+        
+        const getPizzas = async () => {
+            try{
+                const dataPizzas = await (await axios.get(url)).data
+                setItems(dataPizzas)
                 setIsLoading(false)
-            })
+            }catch(error){
+                console.log(error,12121)
+                alert('Ошибка при получение пиццы!');
+            }finally{
+                setIsLoading(false)
+            }
+        }
+        getPizzas()
+
         window.scrollTo(0, 0)
 	}, [categoryId, sort, searchValue, currentPage])
 
